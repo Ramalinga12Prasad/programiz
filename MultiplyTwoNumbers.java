440c241cb92a892717ab963873dd7834a5ab063a
Ramalinga <ram-zt0388@ram-zt0388.csez.zohocorpin.com>
2021-02-03 20:56:43 +0530

    Multiply two float numbers program

---
+++ MultiplyTwoNumbers.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class   MultiplyTwoNumbers{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter two numbers :");
+        float num1 = in.nextFloat();
+        float num2 = in.nextFloat();
+    
+        
+        System.out.println(num1 * num2);
+    }
+}
