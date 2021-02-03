275bb392e08680f26de88b804431b058a243f7e0
Ramalinga <ram-zt0388@ram-zt0388.csez.zohocorpin.com>
2021-02-03 20:50:25 +0530

    Added Print Integer Program

---
+++ PrintInteger.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class   PrintInteger{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter a number :");
+        int num = in.nextInt();
+        System.out.println(num);
+    }
+}
