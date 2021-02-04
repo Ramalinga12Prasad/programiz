fe3bafb1ab643feeea614ebca5d39b76a7a7cc1d
Ramalinga <ram-zt0388@ram-zt0388.csez.zohocorpin.com>
2021-02-04 20:08:31 +0530

    added positive or negative program

---
+++ PositiveOrNegative.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class PositiveOrNegative{
+    public static void main(String args[] ) {
+        Scanner in = new Scanner(System.in);
+        int number = in.nextInt();
+        if(number > 0 ) {
+            System.out.println("Positive");
+        } else if (number < 0) {
+            System.out.println("Negative");
+        } else {
+            System.out.println("Zero");
+        }
+    }
+}
