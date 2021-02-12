4e929a8ef6f8f6039ead1964e1afbcbdcda25df9
ram-zt0388 <ram-zt0388@ram-zt0388.csez.zohocorpin.com>
2021-02-12 19:08:15 +0530

    externalized no of digits to a function

--- NoOfDigits.java
+++ NoOfDigits.java
@@ -3,17 +3,20 @@ import java.util.Scanner;
 //$Id$
 
 public class NoOfDigits {
-
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
-		System.out.println("Enter a number");
-		int num = in.nextInt();
+	public static int digits(int num) {
 		int count =0;
 		while (num>0) {
 			num /= 10;
 			count++;
 		}
-		System.out.println("No of digits " + count);
+		return count;
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int num = in.nextInt();
+		System.out.println("No of digits " + digits(num));
 		
 
 	}
