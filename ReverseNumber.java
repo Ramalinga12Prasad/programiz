80c67bcbd152a245edf57b80322f3e71c24ad3db
ram-zt0388 <ram-zt0388@ram-zt0388.csez.zohocorpin.com>
2021-02-12 18:49:52 +0530

    extracted reverse into a method

--- ReverseNumber.java
+++ ReverseNumber.java
@@ -3,20 +3,22 @@ import java.util.Scanner;
 //$Id$
 
 public class ReverseNumber {
+	public static int reverse(int num) {
+		int rev = 0;
+		int i = 0;
+		while(num > 0) {
+			int rem = num % 10;
+			num /=10;
+			rev = (int) (rev * 10 + rem);
+			}
+		return rev;
+	}
 
 	public static void main(String[] args) {
 	Scanner in = new Scanner(System.in);
 	System.out.println("Enter a number :");
 	int num = in.nextInt();
-	int rev = 0;
-	int i = 0;
-	while(num > 0) {
-		int rem = num % 10;
-		num /=10;
-		rev = (int) (rev * 10 + rem);
-		}
-	
-	System.out.println(rev);
+	System.out.println(reverse(num));
 	
 	
 
